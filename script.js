6b496308b77fd56ae5b03a18c5da360281648bcd
miliweboss <claireholmaert@gmail.com>
2023-09-06 20:52:03 +0200

    fonction fléché

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-// Variables globales
-const span = document.getElementsByTagName('span');
-const btnStart = document.getElementById('start');
-const btnStop = document.getElementById('stop');
-const btnReset = document.getElementById('reset');
-let time;
-let ms = 0;
-let s = 0;
-let min = 0;
-let h = 0;
+document.addEventListener("DOMContentLoaded", () => {
+  const span = document.querySelectorAll(".time-display");
+  const btnStart = document.getElementById("start");
+  const btnStop = document.getElementById("stop");
+  const btnReset = document.getElementById("reset");
+  let time;
+  let ms = 0;
+  let s = 0;
+  let min = 0;
+  let h = 0;
 
-// Fonction pour mettre à jour le compteur
-function update() {
+  // Fonction pour mettre à jour le compteur
+  function update() {
     ms += 1;
     if (ms == 10) {
         ms = 1;
@@ -28,22 +28,19 @@ function update() {
     span[1].innerHTML = min + ' min';
     span[2].innerHTML = s + ' s';
     span[3].innerHTML = ms + ' ms';
-}
+  }
 
-// Gestionnaire d'événement pour le bouton "Start"
-btnStart.addEventListener('click', function () {
+  btnStart.addEventListener("click", () => {
     time = setInterval(update, 100);
     btnStart.disabled = true;
-});
+  });
 
-// Gestionnaire d'événement pour le bouton "Stop"
-btnStop.addEventListener('click', function () {
+  btnStop.addEventListener("click", () => {
     clearInterval(time);
     btnStart.disabled = false;
-});
+  });
 
-// Gestionnaire d'événement pour le bouton "Reset"
-btnReset.addEventListener('click', function () {
+  btnReset.addEventListener("click", () => {
     clearInterval(time);
     btnStart.disabled = false;
     ms = 0;
@@ -54,4 +51,5 @@ btnReset.addEventListener('click', function () {
     span[1].innerHTML = min + ' min';
     span[2].innerHTML = s + ' s';
     span[3].innerHTML = ms + ' ms';
+  });
 });
